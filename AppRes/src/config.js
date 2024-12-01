f724766a954c43f2479ae75c2d99cac3c3210f51
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-12-02 02:40:07 +0530

    Update config.js

--- AppRes/src/config.js
+++ AppRes/src/config.js
@@ -4,8 +4,8 @@ config();
 export const database = {
   connectionLimit: 10,
   host: process.env.DATABASE_HOST || "10.0.64.2",
-  user: process.env.DATABASE_USER || "root",
-  password: process.env.DATABASE_PASSWORD || "JqYo[^In*t9]QSig",
+  user: process.env.DATABASE_USER || "admin_user",
+  password: process.env.DATABASE_PASSWORD || "StrongPassword123",
   database: process.env.DATABASE_NAME || "TestDB",
   port: process.env.DATABASE_PORT || 3306,
 };
